9ecfca8b156f1b0aa5987e612083cb12a0d974e8
rollercoaster671 <60635278+rollercoaster671@users.noreply.github.com>
2022-12-01 21:39:51 -0500

    Add files via upload

--- Roaster_V2_Mega/Roaster_V2_Mega.ino
+++ Roaster_V2_Mega/Roaster_V2_Mega.ino
@@ -8,16 +8,15 @@
 Servo gasvalve;
 Servo ChargeDump;
 
-uint16_t au16data[16] = {
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, -1 };
+uint16_t au16data[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, -1 };
 
-Modbus slave;
+Modbus slave(1,0,0);
 
 int valvepos = 0;
 char  Alarm_String[8] = {0};
 unsigned long flamewaittemp;
 unsigned long flamewaitinf;
-int GasSwitchState = 0;
+boolean GasSwitchState = false;
 int LastGasSwitchState = 0;
 int TimeGasSwitched = 0;
 int GasGracePeriod = 3000; //3 second gas on grace period
@@ -27,34 +26,43 @@ int GasONOFF = 0;
 int ExhaustMaxTemp = 120;
 int LastAlarmSilenceSwitchState = 0;
 int timesilenced = 0;
+int x;
 
-int ChaffPot = A0;
+int ChaffPot = A2;
 int ChaffPotValue = 0;
 int ChaffPotOutput = 0;
-int GasCtrlVlv = A1;
+int ChaffPotPercentage = 0;
+int GasCtrlVlv = A3;
 int GasCtrlVlvValue = 0;
 
 int ChaffCollector = 3;
 
-int ThermoDOamb =5 ;
-int ThermoCSamb =6;
-int ThermoCLKamb =7;
-
+int ThermoDOamb =50 ;
+int ThermoCSamb =9; 
+int ThermoCLKamb =52;
+unsigned int AmbTemp = 0;
+unsigned long AmbTempTot = 0;
 MAX6675 thermocoupleAmb(ThermoCLKamb, ThermoCSamb, ThermoDOamb);
 
-int ThermoDObean =8;
-int ThermoCSbean =9;
-int ThermoCLKbean =10;
+int ThermoDObean =50;
+int ThermoCSbean =33;
+int ThermoCLKbean =52;
+unsigned int BeanTemp = 0;
+unsigned long BeanTempTot = 0;
+MAX6675 thermocoupleBean(ThermoCLKbean, ThermoCSbean, ThermoDObean);
+
 
 int GasValvePin = 11;
 
-int flame_sensor = 12;
+int flame_sensor = 16;
 
-MAX6675 thermocoupleBean(ThermoCLKbean, ThermoCSbean, ThermoDObean);
 
-int ThermoDOexh =22;
+
+int ThermoDOexh =24;
 int ThermoCSexh =23;
-int ThermoCLKexh =24;
+int ThermoCLKexh =22;
+unsigned int ExhTemp = 0;
+unsigned long ExhTempTot = 0;
 
 MAX6675 thermocoupleExh(ThermoCLKexh, ThermoCSexh, ThermoDOexh);
 
@@ -64,24 +72,24 @@ int ThermoCLKfl =27;
 
 MAX6675 thermocoupleFl(ThermoCLKfl, ThermoCSfl, ThermoDOfl);
 
-int GasSolenoid = 28;
+int GasSolenoid = 29;
 
 int StirDir = 29;
 int StirStep = 30;
 
-int ChargeDumpServo = 32;
+//int ChargeDumpServo = 32;
 
 //Switches
 int GasEnableSwitch = 34;
 int DrumAirSwitch = 36;
-int CoolTraySwitch = 38;
+int ArtisanControl = 38;
 
 //LEDs
 int FlameOnLED = 39;
 int FlameAlarmLED = 40;
 int FlameArmedLED = 41;
 int DrumAirLED = 47;
-int CoolingTrayLED = 35;
+//int CoolingTrayLED = 35;
 int AlarmLED = 42;
 
 int AlarmBuzz = 43;
@@ -96,6 +104,7 @@ int AlarmSilence = 46;
 #define motorInterfaceType 1
 
 AccelStepper StirStepper = AccelStepper(motorInterfaceType, StirStep, StirDir);
+
 
 
 int flame_detected;
@@ -114,7 +123,7 @@ void setup() {
   pinMode(flame_sensor, INPUT);
   pinMode(GasEnableSwitch, INPUT_PULLUP);
   pinMode(DrumAirSwitch, INPUT_PULLUP);
-  pinMode(CoolTraySwitch, INPUT_PULLUP);
+  pinMode(ArtisanControl, INPUT_PULLUP);
   pinMode(ChargeButton, INPUT_PULLUP);
   pinMode(AlarmSilence, INPUT_PULLUP);
   pinMode(EStop, INPUT_PULLUP);
@@ -122,19 +131,20 @@ void setup() {
   pinMode(FlameAlarmLED, OUTPUT);
   pinMode(FlameArmedLED, OUTPUT);
   pinMode(DrumAirLED, OUTPUT);
-  pinMode(CoolingTrayLED, OUTPUT);
+//  pinMode(CoolingTrayLED, OUTPUT);
   pinMode(AlarmLED, OUTPUT);
   pinMode(AlarmBuzz, OUTPUT);
   pinMode(GasSolenoid, OUTPUT);
   pinMode(ChaffCollector, OUTPUT);
+
   StirStepper.setMaxSpeed(500);
   StirStepper.setSpeed(200);
   StirStepper.setAcceleration(50);
   gasvalve.attach(GasValvePin);
-  ChargeDump.attach(ChargeDumpServo);
-  slave = Modbus(1,0,0);
-  slave.begin(1920);
-  Serial.begin(9600);
+//  ChargeDump.attach(ChargeDumpServo);
+  
+  slave.begin(19200);
+  //Serial.begin(9600);
 delay(500);  
 }
 
@@ -142,164 +152,145 @@ delay(500);
 
 void loop() {
 
+//Artisan Communication Code--------------------------------------------------
+
+au16data[2] = ((uint16_t) (BeanTemp*100));
+
+au16data[3] = ((uint16_t) (AmbTemp*10));
+//au16data[4] = ((uint16_t) (valvepos*10));
+
+slave.poll( au16data, 16 );
+if (digitalRead(ArtisanControl) == LOW ){  
+   valvepos = 100-(100/90)*au16data[4];
+}
+//_____________________________________________________________________________
+
   GasCtrlVlvValue = analogRead(GasCtrlVlv);
   ChaffPotValue = analogRead(ChaffPot);
   
-  flame_detected = digitalRead(flame_sensor);  
+
 
 //Chaff Collector Fan Control
 
   if(digitalRead(DrumAirSwitch) == LOW) {
+    if(digitalRead(ArtisanControl) == HIGH) {
     ChaffPotOutput = map(ChaffPotValue, 0, 1023, 255, 0);
+    }
+    if(digitalRead(ArtisanControl) == LOW) {
+    ChaffPotOutput = map(au16data[5], 0, 100, 0, 255);
+    }
     analogWrite(ChaffCollector, ChaffPotOutput);
     digitalWrite(DrumAirLED, HIGH);
   }
-  //if(thermocoupleExh.readFarenheit()>(ExhaustMaxTemp+50)) {
-  //  analogWrite(ChaffCollector, 200);
-  //}
+
   if(digitalRead(DrumAirSwitch) == HIGH) {
     analogWrite(ChaffCollector, 0);
+    ChaffPotOutput = 0;
     digitalWrite(DrumAirLED, LOW);
   }
-  Serial.print("DrumAirSwitch =");
-  Serial.println(digitalRead(DrumAirSwitch));
-  Serial.print("ChaffPotValue =");
-  Serial.println(ChaffPotValue);
-  Serial.print("ChaffPotOutput =");
-  Serial.println(ChaffPotOutput);
+    ChaffPotPercentage = ChaffPotOutput/2.55;
+
   
 //______________________________________________________________________________
 
 
 //Gas Control Code---------------------------------------------------------------
    GasSwitchState = digitalRead(GasEnableSwitch);
-   
-   if (au16data[4] == 0) {
-   valvepos = map(GasCtrlVlv, 0, 1023, 0, 90);
+     if (GasSwitchState == LOW) {
+        digitalWrite(GasSolenoid,HIGH);
+     }
+     if(GasSwitchState == HIGH) {
+        digitalWrite(GasSolenoid,LOW);
+        digitalWrite(FlameOnLED,LOW);
+        digitalWrite(FlameAlarmLED,LOW);
+        noTone(AlarmBuzz);
+        TimeGasSwitched = 0;
+     }
+     
+
+   if(digitalRead(ArtisanControl) == HIGH) {
+    valvepos = map(GasCtrlVlvValue, 0, 1023, 0, 90);
    }
-   if (flame_detected == true) {
+
+    flame_detected = digitalRead(flame_sensor);  
+   if (flame_detected == false) {
           digitalWrite(FlameOnLED, HIGH);
    }
-   if(GasSwitchState != LastGasSwitchState) {
-      if(GasSwitchState == LOW){
-        TimeGasSwitched = millis();
-      }}
-
-   if(GasSwitchState == LOW) {
-    
-      if ((millis()-TimeGasSwitched)<GasGracePeriod) {
-          gasvalve.write(GasStartupPos);  
-          digitalWrite(GasSolenoid, HIGH);
-          digitalWrite(FlameArmedLED, HIGH);        
-      }
-      
-      else if ((millis()-TimeGasSwitched)>GasGracePeriod && flame_detected == true) {
-          digitalWrite(GasSolenoid,HIGH);
-          gasvalve.write(valvepos);
-      }
-      
-      else if ((millis()-TimeGasSwitched)>GasGracePeriod && flame_detected == false) {
-          digitalWrite(GasSolenoid, LOW);
-          gasvalve.write(0);
-          digitalWrite(FlameAlarmLED, HIGH);
-          Alarm_String[1] = "Gas Safety Shutoff";
-      }
-        
-      }
-    
-    else {
-    digitalWrite(GasSolenoid, LOW);
-    gasvalve.write(0);
-    digitalWrite(FlameOnLED, LOW);
-    digitalWrite(FlameAlarmLED, LOW);
-    digitalWrite(FlameArmedLED, LOW);
-    Alarm_String[1] = "";
+   if (flame_detected == true) {
+      digitalWrite(FlameOnLED, LOW);
     }
-    
-    //if((millis()-GasGracePeriod-TimeGasSwitched)<GasSafetyPurge) {
-    //        analogWrite(ChaffCollector, 200);
-    //    }
-    LastGasSwitchState = GasSwitchState;
-
    
+
+          gasvalve.write(valvepos);  
 
    
 //_______________________________________________________________________________
 
 //Temperatures-------------------------------------------------------------------------
+
+
+  AmbTempTot = 0;
+  for(x = 0; x<8; x++) {
+    AmbTempTot += thermocoupleAmb.readFarenheit();
+ }
+  AmbTemp = AmbTempTot/8;
+
+  BeanTempTot = 0;
+  for(x = 0; x<8; x++) {
+    BeanTempTot += thermocoupleBean.readFarenheit();
+  }
+  BeanTemp = BeanTempTot/8;
   
-  if(thermocoupleExh.readFarenheit()>ExhaustMaxTemp) {
-    Alarm_String[2] = "HiExhTemp";
-  }
-    if(thermocoupleExh.readFarenheit()>(ExhaustMaxTemp+50)) {
-    Alarm_String[2] = "HiExhTempShutoff";
-  }
+  
 
 
 //_______________________________________________________________________________
 
+//Second Artisan Communication Code--------------------------------------------------
 
-//LCD Code-----------------------------------------------------------------------
-  lcdleft.clear();  
+au16data[2] = ((uint16_t) (BeanTemp*100));
+
+au16data[3] = ((uint16_t) (AmbTemp*10));
+//au16data[4] = ((uint16_t) (valvepos*10));
+
+slave.poll( au16data, 16 );
+if (digitalRead(ArtisanControl) == LOW ){  
+   valvepos = 100-(100/90)*au16data[4];
+} 
+
+//_____________________________________________________________________________
+
+
+
+//LCD Code----------------------------------------------------------------------- 
   lcdleft.setCursor(0, 0);
-  lcdleft.print(String("Bean Temp:") + String(thermocoupleBean.readFarenheit())); 
+  lcdleft.print(String("Bean Temp:") + String(BeanTemp) + String("     ")); 
+//  lcdleft.print(String("Bean Temp:") + String(thermocoupleBean.readFarenheit()) + String("     "));
+  
   lcdleft.setCursor(0, 1); 
-  lcdleft.print(String("Ambient Temp:") + String(thermocoupleAmb.readFarenheit()));
+  lcdleft.print(String("Ambient Temp:") + String(AmbTemp) + String("     "));
+//   lcdleft.print(String("Ambient Temp:") + String(thermocoupleAmb.readFarenheit()) + String("     "));
 
-  lcdright.clear();
   lcdright.setCursor(0, 0); 
   lcdright.print(String("Exhaust Temp:") + String(thermocoupleExh.readFarenheit())); 
   lcdright.setCursor(0, 1); 
   
-  for (int i = 0; i<6; i++) { 
-    lcdright.print(String("Alarm:") + String(Alarm_String[i]));
-  }
+  lcdright.print(String("Gas:") + String(100-valvepos) + String(" Air:") + String(ChaffPotPercentage) + String("     "));
 
 //____________________________________________________________________________
 
 
 //Charge Dump-----------------------------------------------------------------
-    ChargeDump.write(90);
-    if (ChargeButton == LOW) {
+    
+    if (digitalRead(ChargeButton) == LOW) {
       ChargeDump.write(0);
     }
-//____________________________________________________________________________
-
-//Buzzer----------------------------------------------------------------------
-  if(LastAlarmSilenceSwitchState != digitalRead(AlarmSilence)) {
-    if(digitalRead(AlarmSilence) == LOW){
-      timesilenced = millis();
+    else {
+      ChargeDump.write(90);
     }
-  }
-  if(((millis()-timesilenced)>10000) || timesilenced == 0) {
-     for (int i = 0; i<6; i++) { 
-      if(Alarm_String[i] != ""){
-        noTone(AlarmBuzz);
-      }
-     }
-  }
-  else {
-     tone(AlarmBuzz, 50);
-  }
-  //____________________________________________________________________________
-
-//Cooling Tray Code-----------------------------------------------------------
-  if(digitalRead(CoolTraySwitch) == LOW) {
-    digitalWrite(CoolingTrayLED, HIGH);
-    StirStepper.moveTo(5000000);
-    StirStepper.run();
-  } 
 //____________________________________________________________________________
 
-//Artisan Communication Code--------------------------------------------------
-au16data[2] = ((uint16_t) (thermocoupleBean.readFarenheit()*100));
 
-au16data[3] = ((uint16_t) (thermocoupleAmb.readFarenheit()*10));
-
-slave.poll( au16data, 16 );
-  
-   valvepos = 2*au16data[4];
-//_____________________________________________________________________________
 
 
 
